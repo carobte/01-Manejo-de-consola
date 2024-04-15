2f6c4a1fa0ad129dd179182c9b15c0ab2ad893d5
caro.bte <caro.bustamante.escobar@gmail.com>
2024-04-15 11:17:24 -0500

    opciones de console.

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,32 @@
 
 let userName = "Caro"
-let userLastName  = "Bte"
+let userLastName  = "Bustamante"
 const age = "25 years old"
 const userEmail = "caro.bustamante.escobar@gmail.com"
 let userPhone = 3113506770
-let userUbication = "Envigado"
+let userAddress = "Envigado"
 
 console.log(`Hello ${userName} ${userLastName}!, 
 You're ${age}, 
-Your email is: ${userEmail} 
-And your phone is: ${userPhone}. 
-You live in ${userUbication}`)
+Your email is: ${userEmail}, 
+Your phone is: ${userPhone}, 
+You live in ${userAddress}`)
 
 /* 
 
 var: se usaba antes de la actualización de ECMA, ya no se utiliza, 
 let: puede sobreescribir su valor, 
 const: constante, no sobreescribe su valor
+
+*/
+
+/*
+
+console.log() General
+console.info() información, igual al general
+console.warn() advertencia -> naranjadito con !
+console.error() error -> rojo con x
+console.debug () msg para desarrollador, no sale en la consola propiamente, sino en el menu de mensajes -> debug
+console.clear() limpia la consola
 
 */
