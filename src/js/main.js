b2d0ff293f12d3afd9ee6f2c202736bf1a363362
caro.bte <caro.bustamante.escobar@gmail.com>
2024-04-15 11:05:26 -0500

    Diferencias entre var, let y const

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+
+let userName = "Caro"
+let userLastName  = "Bte"
+const age = "25 years old"
+const userEmail = "caro.bustamante.escobar@gmail.com"
+let userPhone = 3113506770
+let userUbication = "Envigado"
+
+console.log(`Hello ${userName} ${userLastName}!, 
+You're ${age}, 
+Your email is: ${userEmail} 
+And your phone is: ${userPhone}. 
+You live in ${userUbication}`)
+
+/* 
+
+var: se usaba antes de la actualización de ECMA, ya no se utiliza, 
+let: puede sobreescribir su valor, 
+const: constante, no sobreescribe su valor
+
+*/
