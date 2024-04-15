24ffba730c879f81a8f05cb112bdd2aa3de4e930
caro.bte <caro.bustamante.escobar@gmail.com>
2024-04-15 11:52:27 -0500

    alerts + console.logs

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,18 @@
 
 let userName = "Caro"
-let userLastName  = "Bustamante"
+let userLastName = "Bustamante"
 const age = "25 years old"
 const userEmail = "caro.bustamante.escobar@gmail.com"
 let userPhone = 3113506770
 let userAddress = "Envigado"
+
+/* Tres formas de escribir console.log */
+console.log("Name: " + userName)
+console.log("Name", userName)
+console.log(`Name: ${userName}`)
+
+console.log("Hello " + userName + " " + userLastName + ", you're " + age + ", your email is: " + userEmail + ", your phone is: " + userPhone + ", you live in " + userAddress)
+console.log("Hello", userName, userLastName, "you're", age, "your email is:", userEmail, "your phone is:", userPhone, "you live in", userAddress)
 
 console.log(`Hello ${userName} ${userLastName}!, 
 You're ${age}, 
@@ -28,5 +36,39 @@ console.warn() advertencia -> naranjadito con !
 console.error() error -> rojo con x
 console.debug () msg para desarrollador, no sale en la consola propiamente, sino en el menu de mensajes -> debug
 console.clear() limpia la consola
+
+- Podemos escribir css dentro de las comillas. Pj:
+
+console.log(`%c${userName}`, "background-color:black; color:white" )
+
+*/
+
+/* Mensajes para el usuario
+
+ alert(userAddress) */
+
+/* SweetAlert2 como opción para alertas estilizadas 
+
+let timerInterval;
+Swal.fire({
+    title: "Auto close alert!",
+    html: "I will close in <b></b> milliseconds.",
+    timer: 2000,
+    timerProgressBar: true,
+    didOpen: () => {
+        Swal.showLoading();
+        const timer = Swal.getPopup().querySelector("b");
+        timerInterval = setInterval(() => {
+            timer.textContent = `${Swal.getTimerLeft()}`;
+        }, 100);
+    },
+    willClose: () => {
+        clearInterval(timerInterval);
+    }
+}).then((result) => {
+    if (result.dismiss === Swal.DismissReason.timer) {
+        console.log("I was closed by the timer");
+    }
+});
 
 */
